8a3c3923927590cb93a56dec6a23c817abb9b12b
golldnet0 <golldnet0@gmail.com>
2019-07-02 21:35:40 -0400

    template, images

--- alienarcade.py
+++ alienarcade.py
@@ -3,11 +3,52 @@ import settings
 """
 Must be ran with sudo on linux.
 """
+class AlienArcade(arcade.Window):
+    def __init__(self):
+        self.ai_settings = settings.Settings()
+        super().__init__(self.ai_settings.screen_width, 
+                         self.ai_settings.screen_height, 
+                         "Alien Arcade")
+        arcade.set_background_color(arcade.color.BLACK)
+        
+
+        #  create sprite lists and set them to none
+
+    def setup(self):
+        #  Create sprites and sprite lists here
+        pass
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        #  Call this function before drawing, it will clear everything but the 
+        #  background color.
+        arcade.start_render()
+
+        #  call all .draw() methods here
+
+    def update(self, delta_time):
+        """
+        All the logic to move and all game logic
+        """
+        pass
+
+    def on_key_press(self, key, key_modifiers):
+        pass
+    
+    def on_key_release(self, key, key_modifiers):
+        pass
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        pass
+
 
 def main():
     print("Prepare for the alien invasion.")
-    ai_settings = settings.Settings()
-    arcade.open_window(ai_settings.screen_width, ai_settings.screen_width, 
-                       "Alien Arcade")
+    game = AlienArcade()
+    arcade.run()
 
-main()
+if __name__ == "__main__":
+    main()
