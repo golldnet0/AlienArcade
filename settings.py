9040b5438ae77f013a166ddcd314bcdcdad3e7b2
Aaron Browne <golldnet0@gmail.com>
2019-07-19 22:27:31 -0400

    adj

--- settings.py
+++ settings.py
@@ -10,15 +10,15 @@ class Settings():
 
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        #  self.bg_color = (230, 230, 230)
 
         #  bullet settings
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (255, 0, 0)
+        #  self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 3
 
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 15
         self.speedup_scale = 1.1
 
         self.initizlize_dynamic_settings()
@@ -26,8 +26,8 @@ class Settings():
     def initizlize_dynamic_settings(self):
         self.ship_speed_factor = 5  # how fast the ship will move 
         self.ship_lives = 2
-        self.bullet_speed_factor = 10
-        self.alien_speed_factor = 1
+        self.bullet_speed_factor = 15
+        self.alien_speed_factor = 2
 
         #  Alien fleet direction. False means the fleet is moving left.
         self.move_fleet_right = True
